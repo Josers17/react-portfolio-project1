6face20995a089c7991438e2d70ab98efd428550
Jose Morales <josea.91@hotmail.com>
2025-10-15 10:42:19 -0400

    Create the Testimony component and the first testimonial block

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Testimony from './components/Testimony';
 
 function App() {
   return (
     <div className="App">
+      <Testimony />
     </div>
   );
 }
---
+++ src/components/Testimony.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Testimony() {
+	return (
+		<div className='testimony-container'>
+			<img 
+				className='testimony-image'
+				src={require('../images/testimony-emma.png')}
+				alt='Photo of Emma'/>
+			<div className='testimony-text-container'>
+				<p className='testimony-name'>Emma Bostian in Sweden</p>
+				<p className='testimony-position'>Software Engineer at Spotify</p>
+				<p className='testimony-text'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+			</div>
+		</div>
+	);
+}
+
+export default Testimony;
